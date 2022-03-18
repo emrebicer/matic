3c58b508f786afcbaeec00602a4864eea9579675
Emre Biçer <oebicer@gmail.com>
2022-03-18 21:20:31 +0300

    remove bias option from linear regression

--- src/linear/regression.rs
+++ src/linear/regression.rs
@@ -3,18 +3,13 @@ use crate::math::linalg::Matrix2d;
 
 pub struct LinearRegression {
     // Weights
-    coefficients: Matrix2d,
-    // Bias
-    intercept: f64,
-    use_intercept: bool,
+    coefficients: Matrix2d
 }
 
 impl LinearRegression {
-    pub fn new(use_intercept: Option<bool>) -> Self {
+    pub fn new() -> Self {
         LinearRegression {
-            coefficients: Matrix2d::new((1, 1)).unwrap(),
-            intercept: 0.,
-            use_intercept: use_intercept.unwrap_or(false),
+            coefficients: Matrix2d::new((1, 1)).unwrap()
         }
     }
     /// Linear regression; calculates the coefficients and intercept that fits
@@ -30,11 +25,16 @@ impl LinearRegression {
     ///
     /// The calculated weights (coefficients) are returned
     ///
-    pub fn fit(&mut self, training_x: &Matrix2d, training_y: &Matrix2d) -> Result<Matrix2d, Error> {
+    pub fn fit(
+        &mut self,
+        training_x: &mut Matrix2d,
+        training_y: &mut Matrix2d,
+    ) -> Result<Matrix2d, Error> {
         if training_x.get_size().0 != training_y.get_size().0 {
-            return Err(Error::InputError("training data size mismatch"));
+            return Err(Error::InputError("training data size mismatch".to_string()));
         }
 
+        // This is the gradient of least squares loss w.r.t. w
         self.coefficients = training_x
             .transpose()?
             .matmul(training_x)?
@@ -45,16 +45,12 @@ impl LinearRegression {
         return Ok(self.coefficients.clone());
     }
 
-    /// Calculates the prediction given weight and input
-    pub fn predict(&self, x: &Matrix2d) -> Result<f64, Error> {
+    /// Calculates the prediction given the input
+    pub fn predict(&self, x: &mut Matrix2d) -> Result<f64, Error> {
         match x.matmul(&self.coefficients) {
             Ok(mult_matrix) => {
                 let mult = mult_matrix.get_elements()[0][0];
-                if self.use_intercept {
-                    Ok(mult + self.intercept)
-                } else {
-                    Ok(mult)
-                }
+                Ok(mult)
             }
             Err(err) => Err(err),
         }
@@ -78,20 +74,20 @@ mod tests {
             vec![4., 5.],
             vec![5., 6.],
         ];
-        let training_x = Matrix2d::from_vec(&training_data_vec).unwrap();
-        let training_y = Matrix2d::from_vec(&vec![vec![5., 8., 11., 14., 17.]])
+        let mut training_x = Matrix2d::from_vec(&training_data_vec).unwrap();
+        let mut training_y = Matrix2d::from_vec(&vec![vec![5., 8., 11., 14., 17.]])
             .unwrap()
             .transpose()
             .unwrap();
 
-        let mut lr = LinearRegression::new(None);
+        let mut lr = LinearRegression::new();
 
-        let coef = lr.fit(&training_x, &training_y).unwrap().get_elements();
+        let coef = lr.fit(&mut training_x, &mut training_y).unwrap().get_elements();
         assert_approx_eq!(coef[0][0], 1.);
         assert_approx_eq!(coef[1][0], 2.);
 
         assert_approx_eq!(
-            lr.predict(&Matrix2d::from_vec(&vec![vec![15., 80.]]).unwrap())
+            lr.predict(&mut Matrix2d::from_vec(&vec![vec![15., 80.]]).unwrap())
                 .unwrap(),
             175.
         );
